a31cd0a3121cc3b8a25d68ce9bf794e091e55b7a
cbarreiro1 <cjbarreiro@vt.edu>
2023-05-20 19:49:05 -0400

    added python files

---
+++ Trading Bot/stream-test.py
@@ -0,0 +1,17 @@
+from alpaca.data.live import CryptoDataStream
+
+API_KEY = 'PKW33XAPHXH0B1L47RJP'
+SECRET_KEY = '67nqMnGmsi67BhZQTWWnQX8rhvw3xMcHrnp9TKJc'
+
+# Initiate class
+crypto_stream = CryptoDataStream(API_KEY, SECRET_KEY)
+
+async def bar_callback(bar):
+    for property_name, value in bar:
+        print(f"\"{property_name}\": {value}")
+
+# Subscribing to bar event 
+symbol = "AAPL"
+crypto_stream.subscribe_bars(bar_callback, symbol)
+
+crypto_stream.run()
