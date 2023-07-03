954db4b5db2537b02a57a4c80b324fcdd6923d17
DurstonS <durstonsnyder@icloud.com>
2023-07-03 09:18:59 -0400

    sdsd
    sd

--- Test.py
+++ Test.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-
 from alpaca.data.live import CryptoDataStream
 
-API_KEY = "PK84NICY437SAQUL8QED"
-SECRET_KEY = "2Wuz5WU20CNbavyzjeiYCt5Udq1owH7g3r0CzVny"
+API_KEY = 'PKW33XAPHXH0B1L47RJP'
+SECRET_KEY = '67nqMnGmsi67BhZQTWWnQX8rhvw3xMcHrnp9TKJc'
 
 # Initiate class
 crypto_stream = CryptoDataStream(API_KEY, SECRET_KEY)
@@ -11,10 +9,11 @@ crypto_stream = CryptoDataStream(API_KEY, SECRET_KEY)
 async def bar_callback(bar):
     for property_name, value in bar:
         print(f"\"{property_name}\": {value}")
-    print("\n")
 
 # Subscribing to bar event 
-symbol = "BTC/USD"
+symbol = "AAPL"
 crypto_stream.subscribe_bars(bar_callback, symbol)
 
 crypto_stream.run()
+
+
