34e31c194863cbd97ee316f5fb64cf49f7a43c85
cbarreiro1 <cjbarreiro@vt.edu>
2023-07-11 21:43:59 -0400

    should have fixed merging problems

--- constants.py
+++ constants.py
@@ -1,1 +1,10 @@
+STOCK_SYMBOLS = ['GDTC', 'BTTX', 'GRRR', 'CING', 'TCBP']  # Top cryptocurrency symbols
+EMA_PERIODS = [12, 26, 9]
+INTERVAL = '15m'  # Interval for price data
+HISTORICAL_PERIOD = '7d'
+BASE_URL = 'https://paper-api.alpaca.markets'  # Paper trading API base URL
 
+# Alpaca API credentials (replace these with your own)
+APCA_API_BASE_URL = 'https://paper-api.alpaca.markets'
+APCA_API_KEY_ID = 'PKD2SNVTL4MK3CWY0Z3F'
+APCA_API_SECRET_KEY = 'ImchqiPGdvb2PKXNaxZhIKIjbEpnuiauTl1R0eVl'
