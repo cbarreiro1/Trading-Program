5d295267be112832982f9647589a1b6aa61aaf7a
DurstonS <133609340+DurstonS@users.noreply.github.com>
2024-05-16 16:29:41 -0400

    Counting correct number of trades but not stopping it from buying

--- alpaca_trading.py
+++ alpaca_trading.py
@@ -3,6 +3,7 @@ from config import APCA_API_BASE_URL, APCA_API_KEY_ID, APCA_API_SECRET_KEY, CODE
 import pandas as pd
 from message import send_text
 from config import Max_Trade_Count
+from datetime import datetime, time
 
 trade_count=0
 
@@ -44,7 +45,7 @@ def buy(symbol:str, price_history:dict) -> bool:
         return False
     
     #Check PDT Rule
-    if trade_count >= Max_Trade_Count
+    if trade_count >= Max_Trade_Count:
        print('Skipping buy order PDT Rule')
        return False
 
@@ -84,3 +85,17 @@ def sell(symbol:str, price_history:dict) -> bool:
 # Get the symbols of stocks that are already held
 def get_held_stocks() -> list:
     return [position.symbol for position in api.list_positions()]
+
+
+def sell_all_positions_before_market_close():
+    current_time = datetime.now().time()
+    # Check if it's 3:55 PM
+    if current_time == time(15, 55):
+        held_stocks = get_held_stocks()
+        if held_stocks:
+            print("Selling all open positions before market close")
+            for symbol in held_stocks:
+                sell(symbol)
+                print(f"Sold all shares of {symbol}")
+        else:
+            print("No open positions to sell.")
--- config.py
+++ config.py
@@ -21,7 +21,7 @@ def update_macd_dict(macd_dict:dict, stocks:list):
             del macd_dict[key]
 
 # Set up variables before running
-CONSTANT_STOCKS = ['CTNT', 'MTC', 'HOLO', 'GME',]
+CONSTANT_STOCKS = ['MNDY', 'MARA', 'CLSK', 'GME',]
 NUMBER_OF_STOCKS = 7
 EXCLUDED_STOCKS = []
 EMA_PERIODS = [12, 26, 9]
@@ -35,9 +35,9 @@ Max_Trade_Count=3
 # Alpaca API credentials
 APCA_API_BASE_URL = 'https://paper-api.alpaca.markets'
 APCA_API_KEY_ID = {'CJ': 'PKHNW562AA4FC6BQ9Y0L',
-                   'Durston': 'PKIN5U1U3QK25MRBURQA'}
+                   'Durston': 'PKH0S7P2ASM6O0FUIMIV'}
 APCA_API_SECRET_KEY = {'CJ': 'aOeNOqIG9ortGxsyxqcpY1rmbk7ZpaftkxGyKzXv',
-                       'Durston': 'Mpmayj2BvMjdtlkM4t6dbS0ONKxub0l1GaL29yEP'}
+                       'Durston': 'PpuaLMAPTC7a1VTuPVonWpzGI8cMUNBxTPksjxw6'}
 
 # Webdriver path on local machine
 WEBDRIVER_PATH = {'CJ': 'C:\\Users\\cjgat\\Downloads\\chromedriver_win32\\chromedriver.exe',
